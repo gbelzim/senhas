f0471b8b3a1115eee5ce24b671e65da4317ba93f
Gabriel H <gabriel.mendes.oliveira01@escola.pr.gov.br>
2024-07-24 16:25:20 -0300

    Add files via upload

---
+++ senhas-main/main.js
@@ -0,0 +1,94 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghjklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() { 
+   if (tamanhoSenha > 1)  {
+      // tamanhoSenha = tamanhoSenha-1;
+      tamanhoSenha--;
+}
+numeroSenha.textContent = tamanhoSenha;
+ geraSenha();
+}
+function aumentaTamanho() {
+  if (tamanhoSenha < 20 ) {
+    // tamanhoSenha = tamanhoSenha+1;
+   tamanhoSenha++;
+}
+numeroSenha.textContent = tamanhoSenha;
+geraSenha();
+
+}
+
+for (i =0; i< checkbox.length;i++) {
+    checkbox[i].onclick = geraSenha;
+}
+
+geraSenha();
+
+function geraSenha() {
+    let alfabeto = '';
+   if (checkbox[0].checked) {
+      alfabeto = alfabeto + letrasMaiuscula;
+   }
+   if (checkbox[1].checked) {
+      alfabeto = alfabeto + letrasMinusculas;
+   }
+   if (checkbox[2].checked) {
+      alfabeto = alfabeto + numeros;
+   }
+   if (checkbox[3].cheked) {
+      alfabeto = alfabeto + simbolos;
+   }
+   let senha = '';
+   for (let i = 0; i < tamanhoSenha; i++) {
+        let nummeroAleatorio = math.random() * alfabeto.length;
+        numeroAleatorio = math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+   }
+   campoSenha.value = senha;
+    classificadoSenha(albeto.length);
+   
+    }
+   
+   function classificaSenha(tamanhoAlfabeto){
+     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto)
+     console.log(entropia);
+     forcaSenha.classList.remove('fraca','media','forte');
+     if (entropia > 57){
+        forcaSenha.classList.add('forte');
+     } else if (entropia > 35 && entropia < 57 ) {
+     }
+
+   }
+
+
+function classificaSenha(tamanhoAlfabeto){
+ let entropia = tamanhoSenha * Math.log2(TamanhoAfabeto);
+console.log(entropia);
+forcaSenha.classList.remove('fraca', 'media','forte');
+if (entropia > 57){
+   forcaSenha.classList.add('forte');
+} else if (entropia > 35 && entropia < 57 ) {
+   forcaSenha.classList.add('media');
+} else if (entropia <= 35) {
+   forcaSenha.classList.add('fraca');
+}
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = "Um computador pode levar até" + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha.";
+
+
+
+
+}
